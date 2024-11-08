d7fb9345466bfe26729995c9dc9308ff538a28ae
sunmin <jsunmin0@gmail.com>
2024-11-08 10:28:40 +0900

    User 도메인 파일 추가

---
+++ src/main/java/me/shinsunyoung/springBootDeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package me.shinsunyoung.springBootDeveloper.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+
+
+}
