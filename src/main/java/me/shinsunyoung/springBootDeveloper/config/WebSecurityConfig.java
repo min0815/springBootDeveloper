d03f48472a387e5892543121220cf24a0565e4f1
sunmin <jsunmin0@gmail.com>
2024-11-11 11:16:08 +0900

    241111 스프링 시큐리티 연습

--- src/main/java/me/shinsunyoung/springBootDeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/shinsunyoung/springBootDeveloper/config/WebSecurityConfig.java
@@ -4,32 +4,45 @@ import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springBootDeveloper.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
 
+    private final UserDetailService userService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/h2-console/**", "/static/**",  "/login", "/signup", "/user")
+                        .requestMatchers("/h2-console/**", "/static/**", "/login", "/signup", "/user")
                         .permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/articles", true));
+                        .defaultSuccessUrl("/articles", true))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager() throws Exception {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+
+        ProviderManager providerManager = new ProviderManager(authenticationProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+
+        return providerManager;
     }
-
-
 }
--- src/main/java/me/shinsunyoung/springBootDeveloper/controller/UserApiController.java
+++ src/main/java/me/shinsunyoung/springBootDeveloper/controller/UserApiController.java
@@ -1,8 +1,12 @@
 package me.shinsunyoung.springBootDeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springBootDeveloper.dto.AddUserRequest;
 import me.shinsunyoung.springBootDeveloper.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,6 +19,13 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, response, authentication);
         return "redirect:/login";
     }
 }
