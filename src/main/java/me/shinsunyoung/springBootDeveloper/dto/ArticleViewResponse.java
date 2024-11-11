fbc04478c6c638d73e42cae3a361d67245cfeb9b
sunmin <jsunmin0@gmail.com>
2024-11-11 16:03:13 +0900

    241111 스프링부트 연습

--- src/main/java/me/shinsunyoung/springBootDeveloper/dto/ArticleViewResponse.java
+++ src/main/java/me/shinsunyoung/springBootDeveloper/dto/ArticleViewResponse.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.shinsunyoung.springBootDeveloper.domain.Article;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Getter
 public class ArticleViewResponse {
@@ -11,10 +13,12 @@ public class ArticleViewResponse {
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
 
     public ArticleViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
     }
 }
